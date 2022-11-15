3f07343d580d94633b5cd8ffc59cfd1f687dbc31
Hadi <hadi.youness@lau.edu>
2022-11-15 22:15:02 +0200

    new database setup commit

--- src/routers/rides.js
+++ src/routers/rides.js
@@ -1,54 +1,95 @@
 import { Router } from "express";
 import { rides, stopRequests } from "../utilities/mock-data.js";
+import createConnection from "../../config/databaseConfig.js";
 
+var connection = createConnection();
 const router = Router();
 
 router.get("/stopRequests", (req, res) => {
-  const { rideID, studentID } = req.query;
-  let output = stopRequests;
+    const { rideID, studentID } = req.query;
+    var query = 'SELECT * FROM STOPREQUEST ';
+    if (rideID || studentID) {
+        query += 'WHERE';
+    }
+    if (rideID) {
+        query += ` rideID = ${rideID}`;
+    }
+    if (rideID && studentID) {
+        query += ' & ';
+    }
+    if (studentID) {
+        query += ` studentID = ${studentID}`;
+    }
+    connection.query(`${query};`, function(error, results) {
+        if (results) {
+            let output = results;
+            res.json(output);
 
-  if (rideID)
-    output = output.filter(
-      (stopRequest) => stopRequest.rideID === parseInt(rideID)
-    );
+        } else {
+            console.log(error);
+        }
+    });
 
-  if (studentID)
-    output = output.filter(
-      (stopRequest) => stopRequest.studentID === parseInt(studentID)
-    );
+    // output = output.filter(
+    //   (stopRequest) => stopRequest.rideID === parseInt(rideID)
+    // );
 
-  res.json(output);
+    // if (studentID)
+    //     output = output.filter(
+    //         (stopRequest) => stopRequest.studentID === parseInt(studentID)
+    //     );
+
 });
 
 router.get("/stopRequests/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id);
+    const query = `SELECT * FROM STOPREQUEST WHERE id = ${id}`;
+    connection.query(`${query};`, function(error, results) {
+        if (results) {
+            let stopRequest = results;
+            if (stopRequest.length > 0) res.status(200).json(stopRequest.pop());
+            else res.status(404).send("Stop request not found.");
+        } else {
+            console.log(error);
+        }
+    });
 
-  const stopRequest = stopRequests.filter(
-    (stopRequest) => stopRequest.id === id
-  );
 
-  if (stopRequest.length > 0) res.status(200).json(stopRequest.pop());
-  else res.status(404).send("Stop request not found.");
 });
 
 router.get("/", (req, res) => {
-  const { driverID } = req.query;
-  let output = rides;
-  if (driverID)
-    output = output.filter(
-      (stopRequest) => stopRequest.driverID === parseInt(driverID)
-    );
+    const { driverID } = req.query;
+    if (driverID) {
+        const query = `SELECT * FROM RIDE WHERE studentID = ${driverID}`;
+        connection.query(`${query};`, function(error, results) {
+            if (results) {
+                let rides = results;
+                if (rides.length > 0) res.status(200).json(rides);
+                else res.status(404).send("No Rides Found.");
 
-  res.json(output);
+            } else {
+                console.log(error);
+            }
+        });
+    } else res.status(404).send('Please select a driver');
+
 });
 
 router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id);
 
-  const ride = rides.filter((ride) => ride.id === id);
+    const query = `SELECT * FROM RIDE WHERE ID = ${id}`;
+    connection.query(`${query};`, function(error, results) {
+        if (results) {
+            let rides = results;
+            if (rides.length > 0) res.status(200).json(rides.pop());
+            else res.status(404).send("No Rides Found.");
 
-  if (ride.length > 0) res.status(200).json(ride.pop());
-  else res.status(404).send("Ride not found.");
+        } else {
+            console.log(error);
+        }
+    });
+
 });
 
 export default router;
