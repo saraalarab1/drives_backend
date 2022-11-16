a0ea46a95df5fcb79d3b9ce3d2f6b9c0fc4ed7b7
Sara Al Arab <sara.alarab@lau.edu>
2022-11-16 23:20:52 +0200

    wip

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -16,12 +16,15 @@ export const getUsers = async(req, res) => {
  
 export const Register = async(req, res) => {
     console.log("********1");
+    console.log(req.body)
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const password = req.body.password; 
+    // const salt = await bcrypt.genSalt();
+    // const hashPassword = await bcrypt.hash(password, salt);
 
-    const { name, email, password } = req.body;
-    const salt = await bcrypt.genSalt();
-    const hashPassword = await bcrypt.hash(password, salt);
-
-    connection.query(`INSERT INTO STUDENT (firstName, universityEmail,password) VALUES (${name},${email},${hashPassword})`, function(error, results) {
+    connection.query(`INSERT INTO STUDENT (firstName,lastName, universityEmail,password, campusID) VALUES ("${firstname}","${lastname}","${email}","${password}",1)`, function(error, results) {
         if (results) {
             console.log(results)
         } else {
@@ -34,48 +37,35 @@ export const Register = async(req, res) => {
 }
  
 export const Login = async(req, res) => {
-    console.log("********2");
-    var user = undefined
-    console.log(req.body.email.value)
-    console.log(req.body.password.value)
-
-    connection.query(`SELECT * FROM STUDENT WHERE universityEmail="${req.body.email.value}"`, function(error, results) {
+    connection.query(`SELECT * FROM STUDENT WHERE universityEmail="${req.body.email}"`, function(error, results) {
         if (results) {
-            console.log(results)
-            users = results;
+            var users = results;
             var user = users.pop()
             if (user.length < 1) res.status(404).send("User not found.");
-            const match =  bcrypt.compare(req.body.password.value, user.password);
+            const match =  req.body.password === user.password;
             if(!match) return res.status(400).json({msg: "Wrong Password"});
             const userId = user.id;
             const firstName = user.firstName;
             const email = user.email;
             const accessToken = jwt.sign({userId, firstName, email}, process.env.ACCESS_TOKEN_SECRET);
             res.status(200).json({ accessToken });
-
         } else {
             console.log(error);
         }
-    });
-
-    console.log(user)
-    
-  
-
+    });    
 }
  
 export const Logout = async(req, res) => {
-    console.log("********3");
     user = req.data
     if(!user[0]) return res.sendStatus(204);
 
-    connection.query(`DELETE FROM STUDENT WHERE ID= ${user[0]}`, function(error, results) {
-        if (results) {
-            user = results;
-        } else {
-            console.log(error);
-        }
-    });
+    // connection.query(`DELETE FROM STUDENT WHERE ID= ${user[0]}`, function(error, results) {
+    //     if (results) {
+    //         user = results;
+    //     } else {
+    //         console.log(error);
+    //     }
+    // });
 
     return res.sendStatus(200);
 }
