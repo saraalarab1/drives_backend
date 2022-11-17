81ff77b389eddd5bf4c3c8d4b00ae2cb81acd311
Sara Al Arab <sara.alarab@lau.edu>
2022-11-17 18:48:18 +0200

    wip authentication

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -13,24 +13,48 @@ export const getUsers = async(req, res) => {
         }
     });
 }
- 
+export const getUniversities = async(req, res) => {
+    connection.query(`SELECT ID,name FROM UNIVERSITY `, function(error, results) {
+        if (results) {
+            console.log(results)
+            const universities = results;
+            res.json(universities);
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+export const getCampuses = async(req, res) => {
+    connection.query(`SELECT ID,name FROM CAMPUS WHERE universityId=${req.params.id} `, function(error, results) {
+        if (results) {
+            console.log(results)
+            const campuses = results;
+            res.json(campuses);
+        } else {
+            console.log(error);
+        }
+    });
+}
+
 export const Register = async(req, res) => {
-    console.log("********1");
-    console.log(req.body)
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
     const email = req.body.email;
+    const phonenumber = req.body.phonenumber;
+    const birthdate = req.birthdate;
     const password = req.body.password; 
     // const salt = await bcrypt.genSalt();
     // const hashPassword = await bcrypt.hash(password, salt);
 
-    connection.query(`INSERT INTO STUDENT (firstName,lastName, universityEmail,password, campusID) VALUES ("${firstname}","${lastname}","${email}","${password}",1)`, function(error, results) {
+    connection.query(`INSERT INTO STUDENT (firstName,lastName,phoneNumber, universityEmail,dateOfBirth,password, campusID) VALUES ("${firstname}","${lastname}",${phonenumber},"${email}","${birthdate}","${password}",1)`, function(error, results) {
         if (results) {
             console.log(results)
         } else {
             console.log(error);
         }
     });
+    
       
     res.json({msg: "Registration Successful"});
 
@@ -44,11 +68,12 @@ export const Login = async(req, res) => {
             if (user.length < 1) res.status(404).send("User not found.");
             const match =  req.body.password === user.password;
             if(!match) return res.status(400).json({msg: "Wrong Password"});
-            const userId = user.id;
+            const userId = user.ID;
             const firstName = user.firstName;
+            const lastName = user.lastName;
             const email = user.email;
             const accessToken = jwt.sign({userId, firstName, email}, process.env.ACCESS_TOKEN_SECRET);
-            res.status(200).json({ accessToken });
+            res.status(200).json({ accessToken,userId,firstName,lastName });
         } else {
             console.log(error);
         }
--- src/routers/authentication.js
+++ src/routers/authentication.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getUsers, Register, Login, Logout } from "../controllers/users.js";
+import { getUsers, Register, Login, Logout, getCampuses, getUniversities } from "../controllers/users.js";
 
 const router = Router();
 
 router.get('/', getUsers);
+router.get(`/:id/campuses`, getCampuses);
+router.get('/universities', getUniversities);
 router.post('/register', Register);
 router.post('/login', Login);
 router.delete('/logout', Logout);
